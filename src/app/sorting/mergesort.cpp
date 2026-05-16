d43e374d0b5659126ffad60bebe097c8e4941a70
geettalati <2340401112@stu.manit.ac.in>
2026-05-16 23:23:42 +0530

    Implement merge logic in MergeSort::recursiveSort for sorting vectors

--- src/app/sorting/mergesort.cpp
+++ src/app/sorting/mergesort.cpp
@@ -14,7 +14,34 @@ void MergeSort::recursiveSort(int left, int right) {
         recursiveSort(left, mid);
         recursiveSort(mid + 1, right);
         // Merge the two sorted halves
+
+        std::vector<int> result;
+        int i = left, j = mid + 1;
+        while (i <= mid && j <= right) {
+            if ((*nums)[i] < (*nums)[j]) {
+                result.push_back((*nums)[i]);
+                i++;
+            } else {
+                result.push_back((*nums)[j]);
+                j++;
+            }
+        } 
+
+        while(i <= mid) {
+            result.push_back((*nums)[i]);
+            i++;
+        }
+        
+        while(j <= right) {
+            result.push_back((*nums)[j]);
+            j++;
+        }
+        for (int k = left; k <= right; k++) {
+            (*nums)[k] = result[k - left];
+        }
+        
     }
+    return;
 }
 
 void MergeSort::sort() {
